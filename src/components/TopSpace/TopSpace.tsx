84473855ba573ecf04f6c8f7ef4bf2ea9a371ef4
Zehua-Chen <peterchen06242000@outlook.com>
2021-11-11 21:07:35 -0500

    [refactor] Migrate TopSpace

--- src/components/TopSpace/TopSpace.tsx
+++ src/components/TopSpace/TopSpace.tsx
@@ -1,18 +1,13 @@
-import React from "react";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-
-const styles = (theme: Theme) =>
-  createStyles({
-    container: {
-      height: theme.spacing(2),
-    },
-  });
-
-const useStyles = makeStyles(styles);
+import { css } from "@emotion/react";
 
 function TopSpace() {
-  const classes = useStyles();
-  return <div className={classes.container}></div>;
+  return (
+    <div
+      css={(theme) => css`
+        height: ${theme.spacing(2)};
+      `}
+    ></div>
+  );
 }
 
 export default TopSpace;
